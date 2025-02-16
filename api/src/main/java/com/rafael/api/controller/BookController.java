a443bb0c2541c8b3191cd238da41a7130f8f79c6
Rafael <rafael_unis@hotmail.com>
2025-02-16 21:26:11 +0000

    Created controller and service to create and search for books

---
+++ api/src/main/java/com/rafael/api/controller/BookController.java
@@ -0,0 +1,34 @@
+package com.rafael.api.controller;
+
+
+import com.rafael.api.model.Book;
+import com.rafael.api.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        Book savedBook = bookService.saveBook(book);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedBook);
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+}
---
+++ api/src/main/java/com/rafael/api/service/BookService.java
@@ -0,0 +1,27 @@
+package com.rafael.api.service;
+
+import com.rafael.api.model.Book;
+import com.rafael.api.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}
