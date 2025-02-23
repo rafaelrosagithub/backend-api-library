e65e0dca9e86ee8e59474ec32b54220a29810c01
Rafael <rafael_unis@hotmail.com>
2025-02-23 21:01:43 +0000

    OpenAI integration improvements

--- api/src/main/java/com/rafael/api/client/AIClient.java
+++ api/src/main/java/com/rafael/api/client/AIClient.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+import java.util.Map;
+
 @Component
 public class AIClient {
 
@@ -25,28 +28,26 @@ public class AIClient {
                 .build();
     }
 
-    public Mono<BookInsightAIResponse> generateInsight(String prompt) {
-        String requestBody = """
-    {
-      "model": "gpt-4o",
-      "messages": [
-        { "role": "system", "content": "You are a helpful assistant." },
-        { "role": "user", "content": "%s" }
-      ]
-    }
-    """.formatted(prompt);
-
+    public Mono<String> generateInsight(String prompt) {
+        Map<String, Object> mapRequestBody = Map.of(
+                "model", "gpt-4o-mini",
+                "store", true,
+                "messages", List.of(
+                        Map.of("role", "user", "content", prompt)
+                )
+        );
+        String jsonRequestBody = JsonStringToObjectParser.toJson(mapRequestBody );
         return webClient.post()
-                .bodyValue(requestBody)
+                .bodyValue(jsonRequestBody)
                 .retrieve()
                 .onStatus(HttpStatusCode::isError, response ->
                         response.bodyToMono(String.class)
                                 .flatMap(errorBody -> {
-                                    String errorMessage = (String) JsonStringToObjectParser.parseJson(errorBody, "message");
+                                    String errorMessage = (String) JsonStringToObjectParser.parseJson(errorBody, "error", "message");
                                     return Mono.error(new IntegrationAIException(errorMessage, response.statusCode().value()));
                                 })
                 )
-                .bodyToMono(BookInsightAIResponse.class);
+                .bodyToMono(String.class);
     }
 }
 
--- api/src/main/java/com/rafael/api/service/BookInsightAIService.java
+++ api/src/main/java/com/rafael/api/service/BookInsightAIService.java
@@ -4,6 +4,7 @@ import com.rafael.api.client.AIClient;
 import com.rafael.api.dto.BookInsightAIResponse;
 import com.rafael.api.exception.IntegrationAIException;
 import com.rafael.api.repository.BookRepository;
+import com.rafael.api.utils.JsonStringToObjectParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -22,10 +23,21 @@ public class BookInsightAIService {
         return Mono.justOrEmpty(bookRepository.findById(id))
                 .switchIfEmpty(Mono.error(new IntegrationAIException("Book not found.", HttpStatus.NOT_FOUND.value())))
                 .flatMap(book -> {
-                    String prompt = book.getDescription() + " " + book.getTitle() + " by " + book.getAuthor();
-
+                    String prompt = """
+                        Generate short and engaging summary:
+                        Title: %s
+                        Author: %s
+                        Description: %s
+                        Respond in the following format:
+                        Summary:  [generated summary]
+                    """.formatted(
+                            (book.getTitle() != null && !book.getTitle().isEmpty()) ? book.getTitle() : "No title provided",
+                            (book.getAuthor() != null && !book.getAuthor().isEmpty()) ? book.getAuthor() : "No author provided",
+                            (book.getDescription() != null && !book.getDescription().isEmpty()) ? book.getDescription() : "No description provided"
+                    );
                     return aiClient.generateInsight(prompt)
-                            .map(aiInsight -> new BookInsightAIResponse(aiInsight.getBook(), aiInsight.getAiInsight()));
+                            .map(aiInsight ->
+                                    new BookInsightAIResponse(book, (String) JsonStringToObjectParser.parseJson(aiInsight, "choices")));
                 });
     }
 
